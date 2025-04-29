0eb9e1b774bef54b890c1cacf868103f6f8e2381
pardipYadav <213sunilyadav@gmail.com>
2025-04-30 05:29:20 +0530

    fetch api

---
+++ practise/fetchapi.js
@@ -0,0 +1,41 @@
+// const userList = document.getElementById("userList");
+
+// const res = fetch("https://jsonplaceholder.typicode.com/users");
+// res
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error("Something went wrongs");
+//     }
+//     return response.json();
+//   })
+//   .then((users) => {
+//     users.forEach((user) => {
+//       console.log(user);
+//       const li = document.createElement("li");
+//       li.textContent = `${user.name} (${user.email}`;
+
+//       userList.appendChild(li);
+//     });
+//   })
+//   .catch((error) => {
+//     userList.innerHTML = `<li style="color:red;">Error: ${error.message}</li>`;
+//   });
+
+const getPlist = async () => {
+  let options = {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+  };
+  const p = await fetch("https://jsonplaceholder.typicode.com/posts", options);
+  let reponse = await p.json();
+  return reponse;
+};
+
+const mainFunc = async () => {
+  let listP = await getPlist();
+  console.log(listP);
+};
+
+mainFunc();

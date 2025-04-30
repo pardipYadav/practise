daf54cd9c780cf8605145201edd36ae639af6cec
pardipYadav <213sunilyadav@gmail.com>
2025-05-01 05:11:28 +0530

    fetch api data by promise

--- practise/fetchapi.js
+++ practise/fetchapi.js
@@ -21,21 +21,32 @@
 //     userList.innerHTML = `<li style="color:red;">Error: ${error.message}</li>`;
 //   });
 
-const getPlist = async () => {
+const getPlist = async (data) => {
   let options = {
     method: "POST",
     headers: {
       "Content-type": "application/json",
     },
+    body: JSON.stringify(data),
   };
   const p = await fetch("https://jsonplaceholder.typicode.com/posts", options);
   let reponse = await p.json();
   return reponse;
 };
-
+const getId = async (id) => {
+  let resp = await fetch("https://jsonplaceholder.typicode.com/posts/" + id);
+  let r = await resp.json();
+  return r;
+};
 const mainFunc = async () => {
-  let listP = await getPlist();
+  let data = {
+    title: "pardeep",
+    body: "bhai",
+    userId: 101,
+  };
+  let listP = await getPlist(data);
   console.log(listP);
+  console.log(await getId(5));
 };
 
 mainFunc();
